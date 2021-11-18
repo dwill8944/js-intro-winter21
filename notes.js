8af73d78ecf86b02c9943aa9b8c889f984204697
Daniel Williams <dwill8944@gmail.com>
2021-11-17 20:00:44 -0700

    added notes on the rest of js

--- notes.js
+++ notes.js
@@ -42,3 +42,84 @@
 // let - things you want to change
 // const - constant or things you don't want change
 // scope is where ever it is you define it in.
+// Datatypes 
+// string "" ''
+// Boolean true false 
+// integer 12312 -123123 0 
+// float 234.234234
+// hash 
+// { first_name: "bob", age: 2 }
+// arr 
+// ['', 324234, []]
+// null 
+// NAN 
+// undefined 
+// + -  / * %
+// ()
+// || && 
+// < > <= >= 
+// =
+// ==
+// ===
+// !=
+// !==
+// ++
+// --
+// +=
+// -=
+// *=
+
+// variables
+// var firstName = 'bob'
+// var first_name = 'bob'
+// var class = 'bob'
+// var true = 'bob'
+// - letters, numbers, _ $ 
+// - has to begin with letter $ _
+// - case sensitive 
+// - stay away from reserve words 
+// - camelcase
+// var FirstName = 'bob'
+// no longer using var becuase of scope
+// var file wide scope 
+// use these instead
+// let - things you want to change
+// const - constant or things you don't want change
+// scope is where ever it is you define it in.
+
+// coding block 
+// {
+
+// }
+
+// if () {
+
+// } else if{
+
+// } else {
+
+// }
+
+// if 
+
+// end
+
+// functions  - method
+// function displayMenu(num) {
+
+// }
+
+// displayMenu(num)
+
+// displayMenu = (args) => {
+
+// }
+
+// const displayMenu = (args) => {
+  
+// }
+
+// displayMenu(num)
+
+// // V function
+// () => code
